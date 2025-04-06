01362d6c0da2a14b16a69c75a8598708fdfd7ab9
enzi <t.enzenebner@gmail.com>
2025-04-06 16:39:04 +0200

    Added VoidPointer

---
+++ NZCore/Utility/VoidPointer.cs
@@ -0,0 +1,11 @@
+// <copyright project="NZCore" file="VoidPointer.cs">
+// Copyright © 2025 Thomas Enzenebner. All rights reserved.
+// </copyright>
+
+namespace NZCore
+{
+    public unsafe struct VoidPointer
+    {
+        public void* Ptr;
+    }
+}
