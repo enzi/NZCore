6b66c65f6460342f6225c7e31d93aab6aa504e7c
enzi <t.enzenebner@gmail.com>
2026-03-25 23:20:45 +0100

    BurstInterOp + UI.Authoring

---
+++ NZCore.UI/BurstInterOp/IModelBinding.cs
@@ -0,0 +1,37 @@
+﻿// <copyright project="NZCore.MVVM" file="IModelBinding.cs" version="1.0.0">
+// Copyright © 2026 Thomas Enzenebner. All rights reserved.
+// </copyright>
+
+using System;
+using System.Runtime.CompilerServices;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+#if UNITY_6000
+namespace NZCore.UI
+{
+    public interface IModelBinding { }
+    public interface IModelBindingNotify : IModelBinding { }
+
+    public static class BindingObjectNotifyDataExtensions
+    {
+        public static unsafe void Notify<T>(this ref T binding, [CallerMemberName] string property = "")
+            where T : unmanaged, IModelBindingNotify
+        {
+            if (BurstUINotify.Notify.Data.IsCreated)
+            {
+                BurstUINotify.Notify.Data.Invoke((IntPtr)UnsafeUtility.AddressOf(ref binding), property);
+            }
+        }
+
+        public static unsafe void NotifyExplicit<T>(this ref T binding, FixedString64Bytes property)
+            where T : unmanaged, IModelBindingNotify
+        {
+            if (BurstUINotify.Notify.Data.IsCreated)
+            {
+                BurstUINotify.Notify.Data.Invoke((IntPtr)UnsafeUtility.AddressOf(ref binding), property);
+            }
+        }
+    }
+}
+#endif
